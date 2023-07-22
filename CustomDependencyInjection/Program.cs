cf5a4aa15ed4198ae8e865f8f0b09eee3c203e97
Bohdan.Kovalevskyi <bogdan931013@gmail.com>
2023-07-22 15:04:10 +0300

    init

---
+++ CustomDependencyInjection/Program.cs
@@ -0,0 +1,59 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace CustomDependencyInjection;
+
+public interface ISingletonService
+{
+    DateTime GetCreationDateTime();
+}
+
+public class SingletonService : ISingletonService
+{
+    private readonly DateTime _creationDateTime = DateTime.Now;
+    
+    public DateTime GetCreationDateTime()
+    {
+        return _creationDateTime;
+    }
+}
+
+public interface IService
+{
+    DateTime GetCreationDateTime();
+}
+
+public class Service : IService
+{
+    private readonly ISingletonService _singletonService;
+
+    public Service(ISingletonService singletonService)
+    {
+        _singletonService = singletonService;
+    }
+
+    public DateTime GetCreationDateTime()
+    {
+        return _singletonService.GetCreationDateTime();
+    }
+}
+
+public static class Program
+{
+    public static async Task Main()
+    {
+        var serviceContainer = new ServiceContainer();
+        
+        serviceContainer.AddSingleton<ISingletonService, SingletonService>();
+        serviceContainer.AddService<IService, Service>();
+
+        var service = serviceContainer.GetService<IService>();
+
+        Console.WriteLine(service?.GetCreationDateTime());
+
+        await Task.Delay(2000);
+        
+        service = serviceContainer.GetService<IService>();
+        
+        Console.WriteLine(service?.GetCreationDateTime());
+    }
+}
---
+++ CustomDependencyInjection/ServiceContainer.cs
@@ -0,0 +1,76 @@
+﻿namespace CustomDependencyInjection;
+
+public class ServiceContainer
+{
+    private readonly Dictionary<Type, Func<object>?> _serviceDictionary = new();
+    private readonly Dictionary<Type, object> _singletonDictionary = new();
+
+    public void AddService<TService, TImpl>() where TImpl : class, TService
+    {
+        _serviceDictionary.Add(typeof(TService), CreateServiceInstance<TImpl>);
+    }
+
+    public void AddSingleton<TService, TImpl>() where TImpl : class, TService
+    {
+        var instance = CreateSingletonInstance<TImpl>();
+        _singletonDictionary.Add(typeof(TService), instance);
+    }
+
+    public T? GetService<T>() where T : class
+    {
+        var type = typeof(T);
+
+        return GetService(type) as T;
+    }
+
+    private object GetService(Type type)
+    {
+        if (_serviceDictionary.TryGetValue(type, out var creator))
+            return creator();
+        else if (_singletonDictionary.TryGetValue(type, out var obj))
+            return obj;
+        else
+            throw new Exception("No registration for " + type);
+    }
+
+    private object CreateServiceInstance<T>() where T : class
+    {
+        var type = typeof(T);
+
+        var ctor = type.GetConstructors().Single();
+
+        var dependencies = ctor
+            .GetParameters()
+            .Select(p => GetService(p.ParameterType))
+            .ToArray();
+
+        return Activator.CreateInstance(type, dependencies);
+    }
+
+    private object CreateSingletonInstance<T>() where T : class
+    {
+        var type = typeof(T);
+
+        return CreateSingletonInstance(type);
+    }
+    
+    private object CreateSingletonInstance(Type type)
+    {
+        var ctor = type.GetConstructors().Single();
+
+        var dependencies = ctor
+            .GetParameters()
+            .Select(p =>
+            {
+                if (_singletonDictionary.TryGetValue(p.ParameterType, out var obj))
+                {
+                    return obj;
+                }
+
+                return CreateSingletonInstance(p.ParameterType);
+            })
+            .ToArray();
+
+        return Activator.CreateInstance(type, dependencies);
+    }
+}
